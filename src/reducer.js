33915bfdb206f7e98845df54ddf3f2f1b1447a6c
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-06 11:32:12 +0200

    Add reducer

---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
+
+export const reducer = combineReducers({
+  comments,
+  users
+});
+
+export default reducer;
+
+//const initialState = {
+//  comments: [],
+//  users: []
+//};
